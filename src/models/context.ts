a2e0496bb9b6509e0330508d914409065a89dea2
Burak Tasci <burak.tasci@cozumtech.net>
2017-10-09 13:26:52 +0300

    feat(afte): add context object members

--- src/models/context.ts
+++ src/models/context.ts
@@ -1,9 +1,48 @@
+import { HttpRequest } from './http-request';
 import { HttpResponse } from './http-response';
 
 /**
- * Represents an Azure Function context.
+ * Represents an HTTP context.
  */
 export interface Context {
-  log(...message: Array<any>): void;
-  done(err: Error | undefined, res: HttpResponse): void;
+  invocationId?: string;
+  executionContext?: any;
+  bindings?: any;
+  req?: HttpRequest;
+  bindingData?: any;
+  res?: HttpResponse;
+
+  /**
+   * Allows writing to the streaming console logs.
+   */
+  log?: {
+    (...message: Array<any>): void,
+    error(...message: Array<any>): void,
+    warn(...message: Array<any>): void,
+    info(...message: Array<any>): void,
+    verbose(...message: Array<any>): void,
+    metric(...message: Array<any>): void,
+  };
+
+  /**
+   * Creates a new bound function.
+   *
+   * @param args
+   */
+  bind?(...args: Array<any>): void;
+
+  /**
+   * Allows writing to the streaming console logs.
+   *
+   * @param message
+   */
+  // log(...message: Array<any>): void;
+
+  /**
+   * Informs the runtime that the function execution has finished.
+   *
+   * @param {Error} err
+   * @param {HttpResponse} propertyBag
+   */
+  done(err?: Error | undefined, propertyBag?: { [key: string]: any }): void;
 }
