6b3914937c9f1c2a384ec681973405631eed5979
Burak Tasci <me@fulls1z3.com>
2017-10-09 13:30:46 +0300

    feat(afte): add http response object members (#14)

--- src/models/http-response.ts
+++ src/models/http-response.ts
@@ -1,9 +1,23 @@
 import { HttpStatusCode } from './http-status-code';
 
 /**
- * Represents a HTTP response including the status code and data.
+ * Represents an HTTP response including the status code and data.
  */
 export interface HttpResponse {
-  status: HttpStatusCode;
+  status: HttpStatusCode | number;
   body: any;
+  headers?: {
+    'content-type'?: string;
+    'content-length'?: HttpStatusCode | number;
+    'content-disposition'?: string;
+    'content-encoding'?: string;
+    'content-language'?: string;
+    'content-range'?: string;
+    'content-location'?: string;
+    'content-md5'?: Buffer;
+    'expires'?: Date;
+    'last-modified'?: Date;
+    [key: string]: any;
+  };
+  isRaw?: boolean;
 }
